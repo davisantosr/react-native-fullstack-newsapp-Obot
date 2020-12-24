801e99bec242872c63305ee1db565530e34cdb3a
davisantosr <davi-rocha2@hotmail.com>
2020-12-24 10:04:56 -0300

    add Provider in the App

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AppLoading from 'expo-app-loading'
-
+import { Provider } from 'react-redux'
 
 import * as Font from 'expo-font';
 
@@ -29,7 +29,9 @@ export default function App() {
   }
   
   return (
-    <AppNavigator/>
+    <Provider store={store}>
+      <AppNavigator/>
+    </Provider>
   );
 }
 
