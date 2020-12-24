4c14d4b03b03f9eb7fea68ff611509153263b45a
davisantosr <davi-rocha2@hotmail.com>
2020-12-24 11:44:00 -0300

    add useDispatch

--- src/screens/NewsListScreen.js
+++ src/screens/NewsListScreen.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { StyleSheet } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux'
 
 import Card from '../components/Card';
+import * as newsActions from '../redux/actions/newsActions';
 
 const NewsAppScreen = props => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(newsActions.fetchArticles())
+
+  }, [dispatch])
+
   return (
       <Card navigation={props.navigation}/>
       
